9f0fd4269311548e8907e50c6e3b4a86ac0842b2
Edelson Barboza <edelsonbsilva@gmail.com>
2022-11-24 18:12:22 -0300

    For / Loop

---
+++ aula-3/script.js
@@ -0,0 +1,10 @@
+/* For loop */
+for(let numero = 0; numero <= 10; numero++){
+    console.log(numero)
+}
+
+const videoGames = ['Switch','PS4','XBox','3DS']
+
+for (let i = 0; i < videoGames.length; i++) {
+    console.log(`O game ${i + 1} é ${videoGames[i]}`)    
+}
